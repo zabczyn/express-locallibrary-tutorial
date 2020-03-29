4047cdc1abae701846098dadfc7710415ac163d0
Martin Wanzek <rashomartin88@protonmail.com>
2020-03-29 02:11:36 +0100

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://LilPeep125:niesamowite19@cluster0-vnrww.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://LilPeep125:niesamowite19@cluster0-vnrww.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
